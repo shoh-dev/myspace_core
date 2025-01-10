412b40fbb1128eafe1d016d7f857950c79dd5266
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2025-01-10 16:26:05 +0500

    update

--- lib/myspace_core.dart
+++ lib/myspace_core.dart
@@ -1,1 +1,2 @@
-
+export 'src/utils/command.dart';
+export 'src/utils/result.dart';
--- lib/src/utils/result.dart
+++ lib/src/utils/result.dart
@@ -2,8 +2,8 @@
 ///
 /// Return a [Result] from a function to indicate success or failure.
 ///
-/// A [Result] is either an [Ok] with a value of type [T]
-/// or an [Error] with an [Exception].
+/// A [Result] is either an [ResultOk] with a value of type [T]
+/// or an [ResultError] with an [Exception].
 ///
 /// Use [Result.ok] to create a successful result with a value of type [T].
 /// Use [Result.error] to create an error result with an [Exception].
@@ -23,30 +23,63 @@ sealed class Result<T> {
   const Result();
 
   /// Creates a successful [Result], completed with the specified [value].
-  const factory Result.ok(T value) = Ok._;
+  const factory Result.ok(T value) = ResultOk._;
 
   /// Creates an error [Result], completed with the specified [error].
-  const factory Result.error(Exception error) = Error._;
+  const factory Result.error(Exception error) = ResultError._;
+
+  @override
+  String toString() => 'Result<$T>';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Result<T>;
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
 }
 
 /// A successful [Result] with a returned [value].
-final class Ok<T> extends Result<T> {
-  const Ok._(this.value);
+final class ResultOk<T> extends Result<T> {
+  const ResultOk._(this.value);
 
   /// The returned value of this result.
   final T value;
 
   @override
   String toString() => 'Result<$T>.ok($value)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ResultOk<T> && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
 }
 
 /// An error [Result] with a resulting [error].
-final class Error<T> extends Result<T> {
-  const Error._(this.error);
+final class ResultError<T> extends Result<T> {
+  const ResultError._(this.error);
 
   /// The resulting error of this result.
   final Exception error;
 
   @override
   String toString() => 'Result<$T>.error($error)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ResultError<T> && other.error == error;
+  }
+
+  @override
+  int get hashCode => error.hashCode;
 }
